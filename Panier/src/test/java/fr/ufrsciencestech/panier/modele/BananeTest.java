be5044088516817a8a3a75dc374ef6152f6cf839
Raphaël C <bulledemousse2@hotmail.Fr>
2018-10-16 15:21:18 +0200

    Update BananeTest.java

--- Panier/src/test/java/fr/ufrsciencestech/panier/modele/BananeTest.java
+++ Panier/src/test/java/fr/ufrsciencestech/panier/modele/BananeTest.java
@@ -36,12 +36,6 @@ public class BananeTest {
     @After
     public void tearDown() {
     }
-
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
     
         @Test
     public void testGetPrix() {
